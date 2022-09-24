3becaebbe5bfba8c7bd2a255b7aceee49771d23b
OdidaProtas <bryodiiidah@gmail.com>
2022-09-24 08:36:52 +0300

    add subtopic

--- src/controller/SubTopicController.ts
+++ src/controller/SubTopicController.ts
@@ -34,7 +34,7 @@ export default class SubTopicController {
         desc: error,
       };
     }
-    return data;
+    return data[0];
   }
 
   async all(request: Request, response: Response, next: NextFunction) {
